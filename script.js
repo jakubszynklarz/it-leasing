964798c872da4d2093cd015c4143e98a317ae4c6
jakubszynklarz <jakubszynklarz@gmail.com>
2018-07-23 20:15:27 +0200

    web

---
+++ script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+    
+    var counter = 0;
+    var c = 0;
+    var i = setInterval(function(){
+
+            $(".loading-page .counter h1").html(c);
+            $(".loading-page").css("width", c + "%");
+
+            counter++;
+            c++;
+            if(counter === 101){
+                clearInterval(i);
+                var loading = document.querySelector(".loading");
+                loading.remove();
+                                
+            }
+            
+        }, 30);
+
+        
+
+});
+
+(function() {
+
+    button = document.querySelector(".backToTop");
+
+    function animateScroll(){
+        if(document.documentElement.scrollTop > 0){
+            window.scrollBy(0, -50);
+            setTimeout(animateScroll, 5);
+        }
+    }
+
+
+    button.addEventListener("click", function(e){
+
+        e.stopPropagation();
+
+        animateScroll();
+
+    }, false);
+    
+
+    window.addEventListener("scroll", function() {
+        
+        if(document.documentElement.scrollTop >= 50) {
+            button.classList.remove("hidden");
+           
+        } else {
+            button.classList.add("hidden");
+            
+        }
+
+    }, false);
+
+
+
+})();
+
+(function() {
+
+    social = document.querySelector(".socialMedia");
+    nav = document.querySelector(".sidebar");
+    nav.addEventListener("mouseover", function(){
+            nav.className = "sidebarOpen";
+            social.classList.remove("hidden");
+    }, false);
+
+    
+    nav.addEventListener("mouseout", function (){
+        nav.className = "sidebar";
+        social.classList.add("hidden");
+    }, false);
+
+
+})();
